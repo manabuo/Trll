3f8b8202b6d2b24a0fe0ce17eae45e92146a452f
Maximo Dominguez <mamodom@gmail.com>
2017-01-26 19:42:29 -0400

    Add project files.

---
+++ Trll.Mobile/Trll.Mobile/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Trll.Mobile.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
